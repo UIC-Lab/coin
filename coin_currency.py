1bc0a0462b0284a76b22e37cbdeb6bbf5ebbb83e
Kim-Gwang-Un <kgo0748@gmail.com>
2018-05-09 18:22:41 +0900

    Repair Directory

--- coin_currency.py
+++ coin_currency.py
@@ -3,6 +3,6 @@ market = coinmarketcap.Market()
 coin = market.ticker('bitcoin', limit=3, convert="KRW")
 coin_dict = coin[0]
 coin_price = coin_dict['price_krw']
-file_opner = open('./coin_currency', 'wt')
+file_opner = open('./coin_currency/bitcoin_price.txt', 'at')
 file_opner.write(coin_price+'\n')
 file_opner.close()
