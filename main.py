a7c6f9279490bcb0e45a9fc47025e0cf9bde5381
Kim-Gwang-Un <kgo0748@gmail.com>
2018-05-31 14:01:00 +0900

    2018 / 05/ 31/ 1400

--- main.py
+++ main.py
@@ -7,17 +7,29 @@ CoinResult = getCoin.getCoinResult()
 Crawl.main(sys.argv)
 NewsResult = IN.input_news()
 
-if CoinResult == "UP":
-    if NewsResult == "UP":
-        result = "FULL PURCHASE"
-    elif NewsResult == "DOWN":
-        result = "HALF SELLING"
-
-elif CoinResult == "DOWN" or CoinResult == "EVEN":
-    if NewsResult == "UP":
-        result = "HALF PURCHASING"
-    elif NewsResult == "DOWN" :
-        result = "FULL SELLING"
 
 
-print(result)
+if CoinResult == 'UP':
+    CoinRate = 0.4
+else :
+    CoinRate = 0.0
+
+if NewsResult == 'UP':
+    NewsRate = 0.6
+else :
+    NewsRate = 0.0
+
+FinalRate = CoinRate + NewsRate
+
+if FinalRate == 1.0:
+    print("FULL PURCHASE")
+elif FinalRate == 0.6 :
+    print("HALF PURCHASE")
+elif FinalRate == 0.4:
+    print("HALF SELLING")
+else :
+    print("FULL SELLING")
+print(CoinResult)
+print(NewsResult)
+print(FinalRate)
+
